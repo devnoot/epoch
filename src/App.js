35137c1d6d04f060cf204848b209034c48441b69
noot <devnoot@gmail.com>
2019-06-19 14:30:28 -0400

    Added change theme button and a theme context provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { ThemeContext, themes } from "./theme-context";
 import "./App.css";
+import { Button } from "@material-ui/core";
 
-const App = ({ header, main, footer }) => {
-  return (
-    <div className="App">
-      <header className="App-header">{header}</header>
-      <main className="App-main">{main}</main>
-      <footer className="App-footer">{footer}</footer>
-    </div>
-  );
-};
+const ChangeThemeButton = props => (
+  <Button variant="contained" onClick={props.changeTheme}>
+    Change Theme
+  </Button>
+);
+class App extends Component {
+  state = {
+    theme: themes.light
+  };
+
+  toggleTheme = () => {
+    this.setState(state => ({
+      theme: state.theme === themes.dark ? themes.light : themes.dark
+    }));
+  };
+
+  render() {
+    const { header, main, footer } = this.props;
+
+    return (
+      <ThemeContext.Provider value={this.state.theme}>
+        <div className="App">
+          <header className="App-header">{header}</header>
+          <ChangeThemeButton changeTheme={this.toggleTheme} />
+          <main className="App-main">{main}</main>
+          <footer className="App-footer">{footer}</footer>
+        </div>
+      </ThemeContext.Provider>
+    );
+  }
+}
 
 export default App;
